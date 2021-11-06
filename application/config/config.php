4866e503ef51f212af21fbd3f18712584f65686a
adamz <58021562+ad4mny@users.noreply.github.com>
2021-11-06 13:50:06 +0800

    updated config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/food-ordering-system/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
