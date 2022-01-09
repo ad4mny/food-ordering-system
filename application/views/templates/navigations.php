91d154b1b1dabef3a617ccd829dd3dbf224baa43
adamz <58021562+ad4mny@users.noreply.github.com>
2022-01-10 05:03:20 +0800

    added new favicon and nav logo

--- application/views/templates/navigations.php
+++ application/views/templates/navigations.php
@@ -1,7 +1,7 @@
 <!-- Navigations -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm sticky-top d-none d-lg-block">
     <div class="container-fluid">
-        <a class="navbar-brand text-success" href="#">eHungry</a>
+        <a class="navbar-brand text-success" href="#"><i class="fas fa-utensils fa-fw"></i> eHungry</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
